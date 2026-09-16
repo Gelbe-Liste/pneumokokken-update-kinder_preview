cbfa1aeed70eb36de5a372c715037182e94fd906
Gelbe-Liste <guido.strehlau@gmail.com>
2026-09-16 17:44:32 +0200

    2026-09-16_pneumokokken-update-kinder_v0.27_Button

--- src/project.js
+++ src/project.js
@@ -24,6 +24,8 @@
  *        Bullet-Struktur auf vier bestehende Textblöcke reduziert.
  * v0.23: Korrekturpaket Kapitel 02/08 inkl. neuer Kachelstruktur, bereinigter Unterzeile,
  *        angepasstem Grafikrahmen sowie Austausch der aktualisierten PNG-Grafiken 02, 04, 05, 08 und 10.
+ * v0.27: Praxis-Workflow Navigation angepasst: aktive Zurück-/Weiter-Buttons gelb,
+ *        deaktivierte Buttons hellgrau; Schriftfarbe durchgehend schwarz.
  * v0.22: Nachkorrekturen umgesetzt: aktualisierte Einstiegs- und Kernaussagen-Texte,
  *        animierte Kennzahlen, hervorgehobene Versorgungslücke, Risikogruppen-Karten,
  *        bereinigter STIKO-Hinweis, runde Workflow-Ziffern sowie Grafik-Update 02/04.
@@ -51,7 +53,7 @@ export const project = {
     analyticsPage: "med-i-scroll-pneumokokken-update-kinder", // Legacy-Alias
     analyticsChapter1: "paediatrie-impfpraevention", // Legacy-Alias
     projectId: "pneumokokken-update-kinder",
-    contentVersion: "v0.25",
+    contentVersion: "v0.27",
     analytics: {
       page: "med.i.scroll | Pneumokokken-Impfung bei Kindern",
       pageType: "Microsite",
